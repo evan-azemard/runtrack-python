a9bcd0f0c69d3e86e229c87763086aa8ae788e00
evan-azemard <evan-azemard@laplateforme.io>
2022-04-21 10:51:27 +0200

    jour02 job01

--- jour02/job01/main.py
+++ jour02/job01/main.py
@@ -1,72 +1,43 @@
 class Personne:
-    """
-    Class Personne
-    """
 
-    def __init__(self, c_prenom, c_nom):
-        print("     création d' un humain")
-        self.prenom = c_prenom
-        self.nom = c_nom
-        print('prénom =>', c_prenom)
-        print("nom", c_nom)
-
-    def setPrenom(self, n):
-        self.__prenom = n
-
-    def getPrenom(self):
-        return self.__prenom
-
-    def setNom(self, n):
-        self.__nom = n
-
-    def getNom(self):
-        return self.__nom
-
-    def display(self):
-        print('     Nouveaux Prénom de h1 =>', self.getPrenom())
-        print('     Nouveaux Nom de h1 =>', self.getNom())
-
-
-print("lancement programe...")
-
-"""
-    classe Livre
-"""
-
-
-class Livre:
-
-    def __init__(self, c_titre):
-        self.titre = c_titre
-
-    def print(self):
-        print('titre', self.titre)
-
-
-l = Livre('test')
-l.print()
-
-"""
-    Class auteur
-"""
+    def __init__(self, nom, prenom):
+        self.nom = nom
+        self.prenom = prenom
 
 
 class Auteur(Personne):
-    def setOeuvre(self, n):
-        self.__oeuvre = n
 
-    def getOeuvre(self):
-        return self.__oeuvre
+    def __init__(self, nom, prenom):
+        self.oeuvre = []
 
     def listerOeuvre(self):
-        print(self.__oeuvre)
+        print(self.oeuvre)
 
-    def ecrireUnLivre(self):
-        def setTitre(self, n):
-            self.__titre = n
+    def ecrireUnLivre(self, titre):
+        livre = Livre(titre, self)
+        self.oeuvre.append(livre.titre)
 
-        def getTitre(self):
-            return self.__titre
 
-# instance = Auteur()
-# instance.ecrireUnLivre()
+class Livre():
+
+    def __init__(self, titre, auteur):
+        self.titre = titre
+        self.auteur = auteur
+
+    # def fprint(self):
+    #     return print(self.titre)
+
+
+# Auteur
+LivreAut = Auteur("Auteur1", "Auteur2")
+
+# print
+# LivreAut.fprint()
+
+# Livre
+LivreAut.ecrireUnLivre("Titre1")
+
+LivreAut.ecrireUnLivre("Titre2")
+
+# Lister
+LivreAut.listerOeuvre()
